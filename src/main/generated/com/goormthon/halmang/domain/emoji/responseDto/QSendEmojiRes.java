422e05db4cf592d8a692cbfac53080f183434d93
sodanhyun <166380533+sodanhyun@users.noreply.github.com>
2024-12-13 08:05:33 +0900

    Update QSendEmojiRes.java

--- src/main/generated/com/goormthon/halmang/domain/emoji/responseDto/QSendEmojiRes.java
+++ src/main/generated/com/goormthon/halmang/domain/emoji/responseDto/QSendEmojiRes.java
@@ -13,10 +13,6 @@ public class QSendEmojiRes extends ConstructorExpression<SendEmojiRes> {
 
     private static final long serialVersionUID = -1800278256L;
 
-    public QSendEmojiRes(com.querydsl.core.types.Expression<String> eId, com.querydsl.core.types.Expression<String> senderId, com.querydsl.core.types.Expression<String> receiverId, com.querydsl.core.types.Expression<java.time.LocalDateTime> regTime) {
-        super(SendEmojiRes.class, new Class<?>[]{String.class, String.class, String.class, java.time.LocalDateTime.class}, eId, senderId, receiverId, regTime);
-    }
-
     public QSendEmojiRes(com.querydsl.core.types.Expression<Long> SendSeq, com.querydsl.core.types.Expression<String> eId, com.querydsl.core.types.Expression<String> senderId, com.querydsl.core.types.Expression<String> receiverId, com.querydsl.core.types.Expression<java.time.LocalDateTime> regTime) {
         super(SendEmojiRes.class, new Class<?>[]{long.class, String.class, String.class, String.class, java.time.LocalDateTime.class}, SendSeq, eId, senderId, receiverId, regTime);
     }
